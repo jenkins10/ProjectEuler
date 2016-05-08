caea8933581bd03ffef58a982c78ba789ff58438
Stephen Jenkins <stephenjenkins@Stephens-MBP.fios-router.home>
2016-05-07 22:56:06 -0400

    Working towards problem 3

--- CPP/Problem3/main.cpp
+++ CPP/Problem3/main.cpp
@@ -1,30 +1,54 @@
 
 #include <iostream>
 
+//#define relevantNumber 600851475143
+#define relevantNumber 10388972
+#define largestNumberToCheck relevantNumber/3
 
 int main()
 {
-  int totalSum = 0;
-  int fibNum = 1;
-  int lastFibNum = 1;
-  int tmp;
-  while( fibNum < 4000000 )
-  {	
-  	tmp = fibNum + lastFibNum;
+  long largestPrime = 1;
+  
 
-    if (tmp % 2 == 0)
-    {
-      totalSum += tmp;
+  bool isPrime[largestNumberToCheck] = {true};
+  std::fill_n(isPrime, largestNumberToCheck, true);
+  // std::cout << isPrime << std::endl;
 
+  // Sieve for Primes
+  for( long i = 2 ; i < largestNumberToCheck/2 ; i++  )
+  { 
+    long iterator = i*2;
+    while ( iterator < largestNumberToCheck )
+    { 
+      // std::cout << iterator << std::endl;
+      isPrime[iterator] = false;
+      iterator += i;
     }
 
-    lastFibNum = fibNum;
-    fibNum = tmp;
   }
 
+  // Check if each prime is a factor of the relevant Number
+  for( long i = 1 ; i < largestNumberToCheck ; i++  )
+  { 
+    // std::cout << isPrime[(int)i] << std::endl;
+    if( isPrime[(int)i]==true )
+    {
+      // std::cout << i << std::endl;
 
-  std::cout << "Sum = " << totalSum << "\n" << std::endl;
+      if(relevantNumber % i == 0)
+      {
+        largestPrime = i;
+      }
+    }
+
+  }
+
+  std::cout << "Largest Prime = " << largestPrime << std::endl;
+
+  return 0;
 }
+
+
 
 
 
